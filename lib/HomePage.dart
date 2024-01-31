a7cda282c7537390f571138741d66a7fd248b672
Victor Schambert <100768297+VictoorS@users.noreply.github.com>
2024-01-31 15:32:34 +0100

    homepage

--- lib/HomePage.dart
+++ lib/HomePage.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/material.dart';
+import 'Models/menu.dart';
+
 
 class HomePage extends StatefulWidget {
+  final String title;
   const HomePage({super.key, required this.title});
 
-  final String title;
 
   @override
   State<HomePage> createState() => _HomePageState();
@@ -11,20 +13,67 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage> {
 
+  var _menus = [
+    Menu(1, 'Collections 2024', 'art.jpg', Color.fromRGBO(212, 175, 55, 1), ),
+  ];
+
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
         title: Text(widget.title),
       ),
-      body: const Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-          ],
+      body:
+      Container(decoration: const BoxDecoration(
+        color: Colors.black
+      ),child: Center(
+        child: ListView.builder(
+          itemCount: _menus.length,
+          itemBuilder: (context, index) => InkWell(
+            onTap: () => null,//Navigator.push(context, MaterialPageRoute(builder: (context) => HomePage)),
+            child: _buildRow(_menus[index]),
+          ),
+          itemExtent:180,
         ),
-      ),
+      ),)
+
     );
   }
+}
+
+@override
+
+_buildRow(Menu menu) {
+  return Container (
+      height: 100,
+      decoration: BoxDecoration(
+        color: menu.color,
+        borderRadius: BorderRadius.all(Radius.circular(20.0)),
+      ),
+      margin: EdgeInsets.all(4.0),
+      child: Column(
+        children: <Widget>[
+          Expanded(
+            child: Image.asset(
+              'assets/images/collections/${menu.image}',
+              fit: BoxFit.fitWidth,
+            ),
+          ),
+          Container(
+            height: 50,
+            child: Center(
+              child: Text(
+                menu.title,
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontFamily: 'Roboto',
+                  fontSize: 28,
+                ),
+              ),
+            ),
+          ),
+        ],
+      )
+  );
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:td_flutter/Models/menu.dart';
 import 'package:td_flutter/HomePage.dart';
 
 void main() {
@@ -8,15 +9,19 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Art by Chiara',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const HomePage(title: 'Flutter Demo Home Page'),
+      home: HomePage(title: "E-Commerce",),
+      debugShowCheckedModeBanner: false,
     );
   }
 }
+
+
